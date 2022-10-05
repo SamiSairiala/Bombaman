24fd4b3d3afba9736c952e744cf0d8428136a9c5
Acey <sami.sairiala@tuni.fi>
2022-10-05 15:27:33 +0300

    Moved damage collision detection to explosions

--- Assets/Code/Explosion.cs
+++ Assets/Code/Explosion.cs
@@ -15,5 +15,20 @@ namespace Bombaman
             Destroy(gameObject, seconds);
         }
 
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                Character player = FindObjectOfType<Character>();
+                player.TakeDamage(Damage);
+                
+                
+            }
+            if(collision.gameObject.tag == "Enemy") // For AI or remember to change this tag to what we call ai's
+            {
+
+            }
+        }
+
     }
 }
