0f5c1d21987932990da52d51ea2c97fcf9c24b58
Acey <sami.sairiala@tuni.fi>
2022-10-06 10:21:58 +0300

    ADD: Camera follow script.
    moved bomb scripts in to their own folder

---
+++ Assets/Code/CameraFollow.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bombaman
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private Transform target;  // Target which camera will follow.
+
+        [SerializeField] private float snaptoTargetTime = 0.2f; // How smoothly it follows.
+
+        private Vector3 velocity;
+        private float zOffset;
+
+        private void Start()
+        {
+            Camera camera = GetComponent<Camera>();
+            if(camera != null)
+            {
+                zOffset = camera.transform.localPosition.z;
+            }
+        }
+
+        private void Update()
+        {
+            Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, zOffset));
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, snaptoTargetTime);
+        }
+    }
+}
